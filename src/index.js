fc00897f63033f3a5d9fe191e181e9fa6ce2b6c6
Maryna K <110280285+Omenat83@users.noreply.github.com>
2024-08-16 18:13:59 +0300

    feat: task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  let result = []
+  if (!matrix) { return [] };
+  result = matrix.flatMap((element, index, arr) => {
+      return index % 2 === 0
+          ? element.sort((a, b) => a - b)
+          : element.sort((a, b) => b - a);
+  });
+  return result;   
 }
